46d7e8efce767248dcc396778f418fa1328277f6
LuckyAndrean_20075 <luckyand222@gmail.com>
2023-09-06 15:07:03 +0700

    push

--- app/page.js
+++ app/page.js
@@ -1,113 +1,137 @@
-import Image from 'next/image'
+"use client";
+import React, { useRef, useState } from "react";
+import Image from "next/image";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import "./globals.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import avatar from "../public/avatar.svg";
+import banner from "../public/img-banner.svg";
+import banner2 from "../public/bannerMobile.svg";
+import Link from "next/link";
 
 export default function Home() {
+  const code = `
+    for (i in range(10)) {
+      print("Slide " + str(i + 1));
+    }
+  `;
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="overflow-x-hidden bg-[#6495ED] md:bg-white">
+      {/* testimoni */}
+      <div className="flex h-auto md:h-[40rem] flex-col items-center justify-start p-10 md:p-20 bg-[#6495ED]">
+        <h3 className="text-2xl font-bold text-white">Testimoni</h3>
+        <div className="px-15 lg:px-52 mt-2">
+          <p className="text-base font-medium text-center text-white">
+            E-Course memberikan bukti nyata keberhasilan dan manfaat yang
+            diperoleh oleh peserta kursus. Lihat bagaimana pengalaman mereka
+            bergabung dalam E-Course
+          </p>
+        </div>
+        <div className="max-w-[400px] md:max-w-[768px] lg:max-w-[1143px] px-5 lg:px-0 flex">
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={20}
+            loop={true}
+            navigation={true}
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 30,
+                loop: true,
+              },
+
+              1024: {
+                slidesPerView: 3,
+                spaceBetween: 40,
+                loop: true,
+              },
+            }}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
+            modules={[Autoplay, Pagination, Navigation]}
+            className="flex flex-col justify-center align-center mt-10"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            {code.split("\n").map((slide, i) => (
+              <SwiperSlide
+                key={i + 1}
+                className="bg-white w-10 text-black h-48 rounded-2xl py-5 px-10"
+              >
+                <div className="flex gap-6 mb-5">
+                  <Image
+                    className="rounded-full"
+                    src={avatar}
+                    width={58}
+                    height={58}
+                    alt="Picture of the author"
+                  />
+                  <div className="flex flex-col">
+                    <h6 className="text-xl font-semibold">Roberto Kalvin</h6>
+
+                    <div className="text-yellow-400 flex gap-3">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-star-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+                      </svg>
+                      <p className="text-sm font-light text-black">4/5</p>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <p className="text-base font-medium text-left">
+                    “Worth it banget belajar disini! Materi sangat lengkap,
+                    dapet sertifikat juga”
+                  </p>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      {/* buat akun */}
+      <div className="flex flex-col items-center justify-start py-2 md:py-10 h-auto">
+        <div className="md:flex max-w-[1143px] w-full md:shadow-lg md:rounded-2xl md:-mt-36  md:bg-white">
+          <div className="w-full md:w-4/6 py-8 px-5 md:pl-10">
+            <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold text-white md:text-black">
+              Buat Akun Sekarang, Gratis!
+            </h2>
+            <div className="w-full md:w-4/6 flex items-end md:pr-2 md:hidden ">
+              <Image src={banner2} alt="banner" />
+            </div>
+            <div className="my-5 pr-10 md:pr-20 lg:pr-40">
+              <p className="text-base font-medium text-left text-white md:text-black">
+                Gabung bersama kami dan dapatkan informasi hingga diskon spesial
+                buat kamu
+              </p>
+            </div>
+            <Link
+              href="/"
+              className="border-2 bg-white md:bg-transparent md:border-black rounded-3xl px-10 py-2 inline-block font-semibold hover:bg-[#6495ED] hover:text-white md:hover:border-white"
+            >
+              Daftar
+            </Link>
+          </div>
+          <div className="w-full md:w-4/6 items-end md:pr-2 hidden md:flex ">
+            <Image className="w-[524px]" src={banner} alt="banner" />
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
